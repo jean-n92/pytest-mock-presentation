38e74807bfe0629c59d14ccb576c6354e9f9d030
Gianmaria Genetlici <g.genetlici@gmail.com>
2021-08-18 00:01:36 +0200

    Adding the spark part

--- neon/functions.py
+++ neon/functions.py
@@ -1,3 +1,4 @@
+import json
 import os
 import random
 import time
@@ -5,32 +6,26 @@ import logging
 import requests
 import names
 
+from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
 
 from dataclasses import dataclass
-from typing import List, Optional
+from typing import List, Optional, Union
 
 LOGGER = logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 PATH = os.path.abspath(os.path.dirname(__file__))
 
 
-@dataclass
-class CatFact:
-    fact: str
-    length: int
-    author: str
-
-
-def make_request(usertime: Optional[int] = None) -> None:
+def make_request(waiting: Optional[int] = None) -> None:
     """
     Mock method that simulates an API request. What it does is just
     putting the application to sleep for few seconds to simulate
     the buffering time of a big payload. The time that needs to
     pass can be passed as optional input.
     """
-    timer = usertime if usertime else random.choice(range(1, 10))
+    timer = waiting if waiting else random.choice(range(1, 10))
     t0 = time.perf_counter()
     time.sleep(timer)
     t1 = time.perf_counter() - t0
@@ -38,31 +33,35 @@ def make_request(usertime: Optional[int] = None) -> None:
     return None
 
 
-def retrieve_data(usernumber: Optional[int] = None,
-                  waiting: Optional[bool] = None) -> List[CatFact]:
+def retrieve_data(waiting: Optional[int] = None) -> dict:
     """
     Function that retrieves a standard JSON content from a public API.
     If no custom url is specified, it will retrieve a certain number of
     custom cat facts. To simulate a slow connection or a very heavy
     data, the make_request method had been embedded.
     """
-    num_requests = usernumber if usernumber else random.choice(range(1, 0))
-    req_collection = []
     req_url = "https://catfact.ninja/fact"
-    for n in range(0, num_requests):
-        logging.debug("Now arranging API call to %s" % req_url)
-        make_request() if waiting else None
-        logging.debug("Starting call now")
-        data = requests.get(url=req_url).json()
-        req_collection.append(
-            CatFact(
-                fact=data["fact"],
-                length=data["length"],
-                author=names.get_full_name()
-            )
-        )
+    logging.debug("Now arranging API call to %s" % req_url)
+    make_request(waiting) if waiting >= 0 else None
+    logging.debug("Starting call now")
+    data = requests.get(url=req_url).json()
+    return data
+
+
+def process_data(usernumber: int = 1,
+                 waiting: int = 0) -> List[dict]:
+    """
+    Function that puts a certain number of requests together.
+    It serves a processing mechanism, and it is build upon the previous ones.
+    It allows the user to specify a certain waiting time, along with a certain
+    number of requests or output style.
+    """
+    req_collection = []
+    for n in range(0, usernumber):
+        data = retrieve_data(waiting)
+        req_collection.append(data)
         logging.debug(
-            "Load {a} of {b} is done".format(a=n+1, b=num_requests))
+            "Load {a} of {b} is done".format(a=n+1, b=usernumber))
     return req_collection
 
 
@@ -72,15 +71,35 @@ def establish_spark():
     It has already in itself the configuration property that
     allows it to use the local repository as warehouse.
     """
+    logging.getLogger("pyspark").setLevel(logging.ERROR)
+    logging.getLogger("py4j").setLevel(logging.ERROR)
+    logging.debug("Now establishing the Spark Session")
     spark = (SparkSession
              .builder
              .appName("Cat facts")
              .config("spark.sql.warehouse.dir", PATH + "/local_warehouse")
              .enableHiveSupport()
              .getOrCreate())
+    logging.debug("Spark session is established")
     return spark
 
 
+def group_and_save(spark: SparkSession, facts: List[dict]):
+    """
+    DOCU
+    """
+    logging.debug(
+        "Grouping cat facts list, {number} elements".format(number=len(facts)))
+    sc = SparkContext.getOrCreate()
+    logging.debug("Spark context retrieved")
+    rdd = sc.parallelize(data)
+    df = (spark.read.json(rdd)
+          .withColumn("load_dts", f.current_timestamp()))
+    return df
+
+
 if __name__ == "__main__":
-    data = retrieve_data(usernumber=1, waiting=False)
-    print(data)
+    data = process_data(usernumber=3, waiting=1)
+    spark = establish_spark()
+    df = group_and_save(spark, data)
+    df.show(10)
