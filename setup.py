24cbbb6803cc1fe4a9673a9e68251ba8502c8454
Gianmaria Genetlici <g.genetlici@gmail.com>
2022-03-12 16:03:58 +0100

    Autoversioning, better setup script and config, deleted requirement files in favor of setup.cfg, adding license file

--- setup.py
+++ setup.py
@@ -1,13 +1,6 @@
 from distutils.core import setup
-from setuptools import find_packages
+import versioneer
 
-setup(name='CatFacts',
-      version='0.1',
-      description='Presentation Cat Facts',
-      author='Gianmaria Genetlici',
-      author_email='gianmaria.genetlici@gmail.com',
-      url='https://github.com/jean-n92/pytest-mock-presentation',
-      packages=find_packages(),
-      requires=['requirements.txt'],
-      entry_points = {'console_scripts': ['cat-facts=neon.main:main']}
-     )
+setup(version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      )
